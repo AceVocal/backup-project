263d65b54b671027818bf8f679d0f2b6b0aec167
Himanshu <himanshugohil234@gmail.com>
2021-04-25 14:42:04 +0530

    first look

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import cv2 as cv
 
-img = cv.imread("naruhina.jpg")
+# video = cv.VideoCapture("love_story.mp4")
+video = cv.VideoCapture(0)
 
-print(img)
-print("-"*50)
-print("The above is the binary form of the image")
+ret, first_frame = video.read()
+ret, second_frame = video.read()
+while True:
+    diff = cv.absdiff(first_frame, second_frame)
+
+    gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (21,21),0)
+
+    thresh = cv.threshold(gray, 70, 255, cv.THRESH_BINARY)[1]
+
+    thresh = cv.dilate(thresh, None, iterations=2)
+    
+    contours,_ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        cv.putText(first_frame, "Status Movement", (10,20), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 3)
+
+    cv.imshow("my video", first_frame)
+    # cv.imshow("my video", diff)
+
+    first_frame = second_frame
+    ret, second_frame = video.read()
+
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+cv.destroyAllWindows()
